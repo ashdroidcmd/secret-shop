fb22eab9081cd7a9f9915f7412e2eb36c5cb89a0
ashdroidcmd <z.wolff25@gmail.com>
2025-06-09 20:46:43 +0800

    mime fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const isProd = process.env.NODE_ENV === "production";
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: isProd ? "/secret-shop/" : "/",
+  base: process.env.VITE_BASE_PATH || "/",
 });
